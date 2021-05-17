6b8a28bdd061ac0da8e4abc0dea77125dbfd4aa1
ST Chiou <sssumusic@gmail.com>
2021-05-17 20:46:39 +0800

    Initial Commit

---
+++ TrainingRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingRecord
+//
+//  Created by 邱宣策 on 2021/5/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
